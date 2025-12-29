6f3bd44db04016d1dde7912302d47a397a7f7f23
rahul <rahul@gmail.com>
2025-12-29 21:48:44 +0530

    update schema & add custom user object

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,17 @@
 const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
 
+
+const UserObject = new GraphQLObjectType({
+    name: "User",
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        email: { type: GraphQLString },
+    }
+});
+
+
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
@@ -10,6 +22,19 @@ const RootQuery = new GraphQLObjectType({
         hello1: {
             type: GraphQLInt,
             resolve: () => 123456789
+        },
+        user: {
+            type: UserObject,
+            args: { id: { type: GraphQLString } },
+            resolve: (parent, args) => {
+                const users = [{ id: "1", name: "Mohit1", age: 22, email: "mohit@gmail.com" },
+                { id: "2", name: "Mohit2", age: 23, email: "mohit@gmail.com" },
+                { id: "3", name: "Mohit3", age: 24, email: "mohit@gmail.com" },
+                { id: "4", name: "Mohit4", age: 25, email: "mohit@gmail.com" },
+                { id: "5", name: "Mohit5", age: 26, email: "mohit@gmail.com" },
+                ]
+                return users.find(user => user.id === args.id)
+            }
         }
     }
 });
