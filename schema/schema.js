4d0d5d1901d822d3cbbea5f51f959b30b7bd7aad
rahul <rahul@gmail.com>
2026-01-13 22:21:46 +0530

    add mutation code

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,11 @@
 const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require("graphql");
 
+const users = [{ id: "1", name: "Mohit1", age: 22 },
+{ id: "2", name: "Mohit2", age: 23 },
+{ id: "3", name: "Mohit3", age: 24 },
+{ id: "4", name: "Mohit4", age: 25 },
+{ id: "5", name: "Mohit5", age: 26 },
+]
 
 const UserObject = new GraphQLObjectType({
     name: "User",
@@ -7,7 +13,6 @@ const UserObject = new GraphQLObjectType({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         age: { type: GraphQLInt },
-        email: { type: GraphQLString },
     }
 });
 
@@ -27,18 +32,79 @@ const RootQuery = new GraphQLObjectType({
             type: UserObject,
             args: { id: { type: GraphQLString } },
             resolve: (parent, args) => {
-                const users = [{ id: "1", name: "Mohit1", age: 22, email: "mohit@gmail.com" },
-                { id: "2", name: "Mohit2", age: 23, email: "mohit@gmail.com" },
-                { id: "3", name: "Mohit3", age: 24, email: "mohit@gmail.com" },
-                { id: "4", name: "Mohit4", age: 25, email: "mohit@gmail.com" },
-                { id: "5", name: "Mohit5", age: 26, email: "mohit@gmail.com" },
-                ]
+                console.log(users)
                 return users.find(user => user.id === args.id)
             }
         }
     }
 });
 
+
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addUser: {
+            type: UserObject,
+            args: {
+                id: { type: GraphQLString },
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => {
+                const user = {
+                    id: users.length + 1 + " ",
+                    name: args.name,
+                    age: args.age
+                }
+                users.push(user)
+                console.log(users)
+                return user
+            }
+        },
+        updateUser: {
+            type: UserObject,
+            args: {
+                id: { type: GraphQLString },
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => {
+                const user = users.find(user => user.id === args.id)
+                if (user) {
+                    user.name = args.name || user.name
+                    user.age = args.age || user.age
+                    console.log(users)
+                    return user
+                }
+                throw new Error("User not found with id " + args.id)
+                /*if (!user) {
+                    throw new Error("User not found with id " + args.id)
+                }
+                user.name = args.name
+                user.age = args.age
+                console.log(users)
+                return user*/
+            }
+        },
+        deleteUser: {
+            type: UserObject,
+            args: { id: { type: GraphQLString } },
+            resolve: (parent, args) => {
+                const userIndex = users.findIndex(user => user.id === args.id)
+                if (userIndex == -1) {
+                    throw new Error("User not found with id " + args.id)
+                }
+                const user = users[userIndex]
+                users.splice(userIndex, 1)
+                console.log(users)
+                return user
+            }
+        }
+
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
